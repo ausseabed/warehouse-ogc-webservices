c6c2b65c7ad47d203b954e2fa058924c52845784
DaveP <david.penton@ga.gov.au>
2020-04-27 03:28:45 +0000

    NGA-282 Align names between catalog and geoserver

--- geoserver/geoserverpush/product_database.py
+++ geoserver/geoserverpush/product_database.py
@@ -1,19 +1,22 @@
-import sys,os
+import sys
+import os
 import requests
 from product_record import ProductRecord
+
 
 class ProductDatabase():
     """ 
     Product Database data structure (at the moment a json formatted file that has the form:
  [
-  {"filename": "s3://bucket-name/name-of-file.tif",
-  "hillshade": "s3://bucket-name/name-of-file.tif",
-  "l0-coverage": "s3://bucket-name/name-of-file.shp"
-  "gazeteer-name":"e.g. Beagle Commonwealth Marine Reserve",
+  {"l3ProductTifLocation": "s3://bucket-name/name-of-file.tif",
+  "hillshadeLocation": "s3://bucket-name/name-of-file.tif",
+  "l0CoverageLocation": "s3://bucket-name/name-of-file.shp"
+  "gazeteerName":"e.g. Beagle Commonwealth Marine Reserve",
   "year":2018,
   "resolution":"1m",
-  "UUID":"68f44afd-78d0-412f-bf9c-9c9fdbe43968"}, ...
+  "UUID":"68f44afd-78d0-412f-bf9c-9c9fdbe43968"}, "metadataPersistentId":"",...
     """
+
     def load_from_commandline(self):
         try:
             self.source_tif_path = os.environ['LIST_PATH']
@@ -21,7 +24,8 @@ class ProductDatabase():
             print("Please set the environment variable LIST_PATH")
             sys.exit(1)
 
-        print("Path to file that specifies what to load (LIST_PATH) = " + self.source_tif_path)
+        print("Path to file that specifies what to load (LIST_PATH) = " +
+              self.source_tif_path)
 
     def get_records(self):
         # Step 1 - read in a list of source tifs
--- geoserver/geoserverpush/product_record.py
+++ geoserver/geoserverpush/product_record.py
@@ -1,40 +1,43 @@
-import sys,os
+import sys
+import os
 import requests
 from geoserver_raster import GeoserverRaster
 from xml.sax.saxutils import escape
+
 
 class ProductRecord():
     """ 
     Product Record data structure (at the moment a json formatted file that has the form:
  [
-  {"filename": "s3://bucket-name/name-of-file.tif",
-  "hillshade": "s3://bucket-name/name-of-file.tif",
-  "l0-coverage": "s3://bucket-name/name-of-file.shp"
-  "gazeteer-name":"e.g. Beagle Commonwealth Marine Reserve",
+  {"l3ProductTifLocation": "s3://bucket-name/name-of-file.tif",
+  "hillshadeLocation": "s3://bucket-name/name-of-file.tif",
+  "l0CoverageLocation": "s3://bucket-name/name-of-file.shp"
+  "gazeteerName":"e.g. Beagle Commonwealth Marine Reserve",
   "srs":"EPSG:32755",
   "year":2018,
   "resolution":"1m",
-  "UUID":"68f44afd-78d0-412f-bf9c-9c9fdbe43968"}, ...
+  "UUID":"68f44afd-78d0-412f-bf9c-9c9fdbe43968"},"metadataPersistentId":"", ...
     """
+
     def __init__(self, input_dictionary):
-        self.filename=input_dictionary["filename"]
-        self.hillshade=input_dictionary["hillshade"]
-        self.l0_coverage=input_dictionary["l0-coverage"]
-        self.gazeteer_name=input_dictionary["gazeteer-name"]
-        self.year=input_dictionary["year"]
-        self.resolution=input_dictionary["resolution"]
-        self.UUID=input_dictionary["UUID"]
-        self.srs=input_dictionary["srs"]
+        self.filename = input_dictionary["l3ProductTifLocation"]
+        self.hillshade = input_dictionary["hillshadeLocation"]
+        self.l0_coverage = input_dictionary["l0CoverageLocation"]
+        self.gazeteer_name = input_dictionary["gazeteerName"]
+        self.year = input_dictionary["year"]
+        self.resolution = input_dictionary["resolution"]
+        self.UUID = input_dictionary["UUID"]
+        self.srs = input_dictionary["srs"]
 
     def get_bathymetric_raster(self):
         geoserver_bath_raster = GeoserverRaster()
         geoserver_bath_raster.load_bath_from_database_entry(self)
-        return geoserver_bath_raster 
+        return geoserver_bath_raster
 
     def get_hillshade_raster(self):
         geoserver_bath_raster = GeoserverRaster()
         geoserver_bath_raster.load_hillshade_from_database_entry(self)
-        return geoserver_bath_raster 
+        return geoserver_bath_raster
 
     def get_l0_coverage_name(self):
         return("{0} {1} {2} L0 Coverage".format(
