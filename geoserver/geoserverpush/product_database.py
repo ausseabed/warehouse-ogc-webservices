220f7cec66f2898dd12e2060101c67f48de9d8f9
DaveP <david.penton@ga.gov.au>
2020-07-27 01:44:39 +0000

    NGA-383: Generalise product database for pipelines

--- geoserver/geoserverpush/product_database.py
+++ geoserver/geoserverpush/product_database.py
@@ -4,7 +4,7 @@ import requests
 import logging
 import product_catalogue_py_rest_client
 from product_catalogue_py_rest_client.rest import ApiException
-from product_catalogue_py_rest_client.models import ProductL3Dist, RelationSummaryDto, Survey
+from product_catalogue_py_rest_client.models import ProductL3Dist, ProductL3Src, RelationSummaryDto, Survey
 from typing import List
 from xml.sax.saxutils import escape
 import re
@@ -27,8 +27,11 @@ class ProductDatabase():
     # survey_l3_relations   : List[RelationSummaryDto]
     # surveys   : List[Survey]
 
-    def __init__(self, bearer_id):
+    def __init__(self, bearer_id, database_url=""):
         self.bearer_id = bearer_id
+        self.source_tif_path = database_url
+        self.snapshot_iso_datetime = str(
+            datetime.utcnow().isoformat())
 
     def load_from_commandline(self):
         try:
@@ -43,8 +46,6 @@ class ProductDatabase():
         try:
             self.snapshot_iso_datetime = os.environ['SNAPSHOT_ISO_DATETIME']
         except KeyError:
-            self.snapshot_iso_datetime = str(
-                datetime.utcnow().isoformat())
             logging.warning(
                 "Please set the environment variable SNAPSHOT_ISO_DATETIME. Using: " + self.snapshot_iso_datetime)
 
@@ -52,13 +53,32 @@ class ProductDatabase():
                      self.snapshot_iso_datetime)
 
     def download_from_rest(self):
-        self.l3_products = self.retrieve_l3_products_using_rest()
+        self.l3_dist_products = self.retrieve_l3_products_using_rest()
+        self.l3_src_products = self.retrieve_l3_src_products_using_rest()
         self.survey_l3_relations = self.retrieve_survey_l3_relations()
         self.surveys = self.retrieve_surveys()
         self.create_label_base_name()
         self.create_raster_base_name()
         self.create_survey_label_base_name()
         self.create_survey_raster_base_name()
+
+    def retrieve_l3_src_products_using_rest(self) -> List[ProductL3Src]:
+        configuration = product_catalogue_py_rest_client.Configuration(
+            host=self.source_tif_path
+        )
+        configuration.access_token = self.bearer_id
+        with product_catalogue_py_rest_client.ApiClient(configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = product_catalogue_py_rest_client.ProductsL3SrcApi(
+                api_client)
+
+            try:
+                api_response = api_instance.products_l3_src_controller_find_all()
+                # logging.info(api_response)
+                return api_response
+            except ApiException as e:
+                logging.error(
+                    "Exception when calling CompilationsApi->compilations_controller_create: %s\n" % e)
 
     def retrieve_l3_products_using_rest(self) -> List[ProductL3Dist]:
         configuration = product_catalogue_py_rest_client.Configuration(
@@ -130,22 +150,22 @@ class ProductDatabase():
                            for (x_id, x_name) in self.base_label_names.items()}
 
     def create_label_base_name(self):
-        self.base_label_names = {x.id: "{0} {1} {2}".format(x.source_product.name, z.year, x.source_product.resolution)
-                                 for x in self.l3_products
+        self.base_label_names = {x.id: "{0} {1} {2}".format(x.name, z.year, x.resolution)
+                                 for x in self.l3_src_products
                                  for y in self.survey_l3_relations
                                  for z in self.surveys
-                                 if x.source_product.id == y.product_id and y.survey_id == z.id}
+                                 if x.id == y.product_id and y.survey_id == z.id}
 
     def create_survey_raster_base_name(self):
         self.survey_base_names = {x_id: re.sub("^[^"+self.SLASH_I_LESS_COLON+"]", "_", re.sub("[^"+self.SLASH_C_LESS_COLON+"]", "_", x_name))
                                   for (x_id, x_name) in self.survey_base_label_names.items()}
 
     def create_survey_label_base_name(self):
-        self.survey_base_label_names = {x.id: "{0} {1} {2}".format(z.name, z.year, x.source_product.resolution)
-                                        for x in self.l3_products
+        self.survey_base_label_names = {x.id: "{0} {1} {2}".format(z.name, z.year, x.resolution)
+                                        for x in self.l3_src_products
                                         for y in self.survey_l3_relations
                                         for z in self.surveys
-                                        if x.source_product.id == y.product_id and y.survey_id == z.id}
+                                        if x.id == y.product_id and y.survey_id == z.id}
 
     def get_survey_names(self):
         return set(zip(self.survey_base_names.values(), self.survey_base_label_names.values()))
@@ -155,16 +175,30 @@ class ProductDatabase():
 
     def get_products_for_survey_name(self, survey_name):
         matching_ids = self.get_product_ids_for_survey_name(survey_name)
-        return [product for product in self.l3_products if product.id in matching_ids]
+        return [product for product in self.l3_src_products if product.id in matching_ids]
 
     def get_label_for_product(self, l3_product: ProductL3Dist, format_string):
+        if not(l3_product in self.l3_dist_products):
+            logging.error(
+                "Expecting to match product {} with survey".format(l3_product.id))
+        else:
+            return get_label_for_product_src(l3_product.source_product)
+
+    def get_name_for_product(self, l3_product: ProductL3Dist, format_string):
+        if not(l3_product in self.l3_dist_products):
+            logging.error(
+                "Expecting to match product {} with survey".format(l3_product.id))
+        else:
+            return get_name_for_product(l3_product.source_product)
+
+    def get_label_for_product_src(self, l3_product: ProductL3Src, format_string):
         if not(l3_product.id in self.base_label_names):
             logging.error(
                 "Expecting to match product {} with survey".format(l3_product.id))
         else:
             return escape(format_string.format(self.base_label_names[l3_product.id]))
 
-    def get_name_for_product(self, l3_product: ProductL3Dist, format_string):
+    def get_name_for_product_src(self, l3_product: ProductL3Src, format_string):
         if not(l3_product.id in self.base_names):
             logging.error(
                 "Expecting to match product {} with survey".format(l3_product.id))
