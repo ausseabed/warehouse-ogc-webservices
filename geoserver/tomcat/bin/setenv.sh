33b524d24c66c501429d657d4d95f4ae3c38a291
DaveP <david.penton@ga.gov.au>
2020-09-28 02:31:05 +0000

    NGA-733 - Another attempt to fix log4j error

--- geoserver/tomcat/bin/setenv.sh
+++ geoserver/tomcat/bin/setenv.sh
@@ -1,3 +1,5 @@
 export JACKSON_VERSION=2.10.1 # MATCH the version used by geoserver
 export CLASSPATH=${CATALINA_HOME}/lib/jackson-annotations-${JACKSON_VERSION}.jar:${CATALINA_HOME}/lib/jackson-core-${JACKSON_VERSION}.jar:${CATALINA_HOME}/lib/jackson-databind-${JACKSON_VERSION}.jar:${CATALINA_HOME}/lib/jul-jsonformatter-1.0.1.jar
 export CATALINA_LOGGING_CONFIG="-Djava.util.logging.config.file=${CATALINA_HOME}/conf/logging.properties"
+
+cat ${CATALINA_HOME}/log4j.properties
