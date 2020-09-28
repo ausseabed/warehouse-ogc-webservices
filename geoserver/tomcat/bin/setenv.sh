7078a779739c6a2a5d315d4ab5cd7e941cec0145
DaveP <david.penton@ga.gov.au>
2020-09-28 00:33:45 +0000

    NGA-733 -use geoserver jackson ver in json logger

--- geoserver/tomcat/bin/setenv.sh
+++ geoserver/tomcat/bin/setenv.sh
@@ -1,1 +1,2 @@
-export CLASSPATH=${CATALINA_HOME}/lib/jackson-annotations-2.10.2.jar:${CATALINA_HOME}/lib/jackson-core-2.10.2.jar:${CATALINA_HOME}/lib/jackson-databind-2.10.2.jar:${CATALINA_HOME}/lib/jul-jsonformatter-1.0.1.jar
+export JACKSON_VERSION=2.10.1 # MATCH the version used by geoserver
+export CLASSPATH=${CATALINA_HOME}/lib/jackson-annotations-${JACKSON_VERSION}.jar:${CATALINA_HOME}/lib/jackson-core-${JACKSON_VERSION}.jar:${CATALINA_HOME}/lib/jackson-databind-${JACKSON_VERSION}.jar:${CATALINA_HOME}/lib/jul-jsonformatter-1.0.1.jar
