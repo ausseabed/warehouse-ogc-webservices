1f225bca3958de8855bcdd662320ee83c8699d9c
DaveP <david.penton@ga.gov.au>
2020-10-25 23:48:10 +0000

    NGA-692: full path to decode_json

--- geoserver/geoserverpush/decode_aws_json.sh
+++ geoserver/geoserverpush/decode_aws_json.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # FARGATE secrets are exported as JSON: {"TF_VAR_geoserver_admin_password":"..."}
 # Convert to normal exports
-export GEOSERVER_ADMIN_PASSWORD=`python3 decode_json.py`
+export GEOSERVER_ADMIN_PASSWORD=`python3 /usr/local/pulldata/decode_json.py`
 $@
