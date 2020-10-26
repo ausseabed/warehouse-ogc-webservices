3adc9ed4f4c59d35c8f22e700de5477f3399f60b
DaveP <david.penton@ga.gov.au>
2020-10-26 03:08:27 +0000

    NGA-692: Add doc to trigger build

--- geoserver/geoserverpush/decode_aws_json.sh
+++ geoserver/geoserverpush/decode_aws_json.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # FARGATE secrets are exported as JSON: {"TF_VAR_geoserver_admin_password":"..."}
-# Convert to normal exports
+# Convert to normal exports and run script
+# e.g. /usr/local/pulldata/decode_aws_json.sh /scripts/entrypoint.sh
 export GEOSERVER_ADMIN_PASSWORD=`python3 /usr/local/pulldata/decode_json.py`
 $@
