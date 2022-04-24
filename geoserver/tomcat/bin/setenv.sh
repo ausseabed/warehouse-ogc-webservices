4044107962c95a505ee18bbc7e89564b0086aceb
Callum McKenna <callum.mckenna@ga.gov.au>
2022-04-25 09:31:57 +1000

    Geoserver 2.20.4 upgrade fixes
    * Explicitly set the admin username
    * Set patch version on jackson-databind
    * Explicitly set javax.xml.stream.XMLOutputFactory provider

--- geoserver/tomcat/bin/setenv.sh
+++ geoserver/tomcat/bin/setenv.sh
@@ -1,4 +1,5 @@
 export JACKSON_VERSION=2.13.2 # MATCH the version used by geoserver
-export CLASSPATH=${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/commons-logging-1.1.1.jar:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/jackson-annotations-${JACKSON_VERSION}.jar:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/jackson-core-${JACKSON_VERSION}.jar:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/jackson-databind-${JACKSON_VERSION}.jar:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/jul-jsonformatter-1.0.1.jar
+export JACKSON_DATABIND_VERSION=2.13.2.1 # MATCH the version used by geoserver
+export CLASSPATH=${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/commons-logging-1.1.1.jar:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/jackson-annotations-${JACKSON_VERSION}.jar:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/jackson-core-${JACKSON_VERSION}.jar:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/jackson-databind-${JACKSON_DATABIND_VERSION}.jar:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/jul-jsonformatter-1.0.1.jar
 export CATALINA_LOGGING_CONFIG="-Djava.util.logging.config.file=${CATALINA_HOME}/conf/logging.properties"
 export TOMCAT_POST_INIT_SCRIPT='/usr/local/pulldata/push_geoserver_settings.py'
